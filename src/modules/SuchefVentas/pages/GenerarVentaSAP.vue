cd4c7fefd5261834c419c408095b55bea7e11502
DevJean93 <jruanoc93@gmail.com>
2024-02-29 10:53:13 -0600

    [ADD]Estilo Toolbar ventas excel

--- src/modules/SuchefVentas/pages/GenerarVentaSAP.vue
+++ src/modules/SuchefVentas/pages/GenerarVentaSAP.vue
@@ -6,7 +6,6 @@ import APISAP from '../../../api/SuchefAPI/APISAP';
 import { ref, onMounted } from 'vue';
 // COMPONENTES PRIMEVUE
 import Card from 'primevue/card';
-import Toolbar from 'primevue/toolbar'
 import Button from 'primevue/button';
 import Dropdown from 'primevue/dropdown';
 import Calendar from 'primevue/calendar';
@@ -19,7 +18,7 @@ const tienda = useTienda()
 const { ListadoTiendas } = storeToRefs(tienda)
 
 // COMPOSABLES
-import { crearObjetoDropdown, GenerarFecha , ObtenerFechaActual } from '../../../composables/Generales'
+import { crearObjetoDropdown, GenerarFecha, ObtenerFechaActual } from '../../../composables/Generales'
 import { MensajeAlertaAuth } from '../../../composables/MensajeAlerta';
 
 // Select Tiendas
@@ -28,29 +27,36 @@ const icondisplay = ref();
 const OptionsTiendas = ref()
 const VentasLoading = ref(false)
 
+// Funcion para descargar las ventas segun tienda seleccionada. 
 const DescargarVentas = async () => {
     VentasLoading.value = true
-    await APISAP.get('Ventas/GenerarExcelVentas', {
-        params: {
-            Tienda: SelectedTienda.value.value,
-            FechaIni: GenerarFecha(icondisplay.value[0]),
-            FechaFin: GenerarFecha(icondisplay.value[1])
-        },
-        responseType: 'blob'
-    })
-        .then((response) => {
-            const url = window.URL.createObjectURL(new Blob([response.data]));
-            const link = document.createElement('a');
-            link.href = url;
-            link.setAttribute('download', `Ventas${ObtenerFechaActual()}.xlsx`);
-            document.body.appendChild(link);
-            link.click();
-            VentasLoading.value = false
+    if (SelectedTienda.value != null) {
+        await APISAP.get('Ventas/GenerarExcelVentas', {
+            params: {
+                Tienda: SelectedTienda.value.value,
+                FechaIni: GenerarFecha(icondisplay.value[0]),
+                FechaFin: GenerarFecha(icondisplay.value[1])
+            },
+            responseType: 'blob'
         })
-        .catch((error) => {
-            VentasLoading.value = false
-            console.error('Error al descargar el archivo', error);
-        });
+            .then((response) => {
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', `Ventas${ObtenerFechaActual()}.xlsx`);
+                document.body.appendChild(link);
+                link.click();
+                VentasLoading.value = false
+            })
+            .catch((error) => {
+                VentasLoading.value = false
+                console.error('Error al descargar el archivo', error);
+            });
+    } else {
+        MensajeAlertaAuth('warning', 'Seleccione una sucursal para continuar', 'Error de Sucursal')
+        VentasLoading.value = false
+    }
+
 }
 
 onMounted(async () => {
@@ -69,51 +75,123 @@ onMounted(async () => {
 <template>
     <Card>
         <template #content>
-            <Toolbar class="mb-4">
-                <template v-slot:start>
-                    <div class="my-2">
-                        <span class="p-float-label">
-                            <Dropdown v-model="SelectedTienda" id="dropTiendas" :options="OptionsTiendas" filter
-                                resetFilterOnHide optionLabel="label" placeholder="Selecccionar Sucursal" class="mr-2">
-                                <template #value="slotProps">
-                                    <div v-if="slotProps.value" class="flex align-items-center">
-                                        <div>{{ slotProps.value.label }}</div>
-                                    </div>
-                                    <span v-else>
-                                        {{ slotProps.placeholder }}
-                                    </span>
-                                </template>
-                                <template #option="slotProps">
-                                    <div class="flex align-items-center">
-                                        <div>{{ slotProps.option.label }}</div>
-                                    </div>
-                                </template>
-                            </Dropdown>
-                            <label for="dropTiendas">Sucursales</label>
-                        </span>
-                        <!-- <Button label="Crear" icon="pi pi-plus" class="p-button-success mr-2" @click="openNew" /> -->
+            <Card class="mb-4 CardStyle">
+                <template #content>
+                    <div class="contenido">
+                        <div class="seccion1">
+                            <span class="p-float-label">
+                                <Dropdown v-model="SelectedTienda" id="dropTiendas" :options="OptionsTiendas" filter
+                                    resetFilterOnHide optionLabel="label" placeholder="Selecccionar Sucursal"
+                                    class="mr-2 DropStyle">
+                                    <template #value="slotProps">
+                                        <div v-if="slotProps.value" class="flex align-items-center">
+                                            <div>{{ slotProps.value.label }}</div>
+                                        </div>
+                                        <span v-else>
+                                            {{ slotProps.placeholder }}
+                                        </span>
+                                    </template>
+                                    <template #option="slotProps">
+                                        <div class="flex align-items-center">
+                                            <div>{{ slotProps.option.label }}</div>
+                                        </div>
+                                    </template>
+                                </Dropdown>
+                                <label for="dropTiendas">Sucursales</label>
+                            </span>
+                            <span class="p-float-label">
+                                <Calendar inputId="calendar" dateFormat="dd/mm/yy" v-model="icondisplay"
+                                    selectionMode="range" :manualInput="false" showIcon iconDisplay="input" class="mr-2">
+                                </Calendar>
+                                <label for="calendar">Rango de Fechas</label>
+                            </span>
+                        </div>
+                        <div class="seccion2">
+                            <Button label="Buscar" icon="pi pi-search" class="p-button-primary mr-2" />
+                            <Button label="Descargar Excel" icon="pi pi-cloud-download" :loading="VentasLoading"
+                                class="p-button-help" severity="success" @click="DescargarVentas" />
+                        </div>
+
                     </div>
-
-                    <span class="p-float-label">
-                        <Calendar inputId="calendar"  dateFormat="dd/mm/yy" v-model="icondisplay" selectionMode="range" :manualInput="false"
-                            showIcon iconDisplay="input" class="mr-2">
-                        </Calendar>
-                        <label for="calendar">Rango de Fechas</label>
-                    </span>
-                    <Button label="Buscar" icon="pi pi-search" class="p-button-primary mr-2" />
-
+                    <!-- <Button label="Crear" icon="pi pi-plus" class="p-button-success mr-2" @click="openNew" /> -->
                 </template>
-
-                <template v-slot:end>
-                    <Button label="Descargar Excel" icon="pi pi-cloud-download" :loading="VentasLoading" class="p-button-help"
-                        @click="DescargarVentas" />
-                </template>
-            </Toolbar>
-
-
+            </Card>
         </template>
 
     </Card>
 </template>
 
-<style scoped></style>
+<style scoped>
+.DropStyle {
+    max-width: 300px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+
+.p-calendar {
+    width: 100%;
+}
+.p-dropdown {
+    width: 100%;
+}
+
+.CardStyle {
+    background: #f9fafb;
+    border: 1px solid #e5e7eb;
+    border-radius: 6px;
+    box-shadow: none;
+}
+
+.contenido {
+    display: flex;
+    flex-direction: row;
+    gap: 7px;
+}
+
+.seccion1 {
+    gap: 7px;
+    width: max-content;
+    display: flex;
+    flex-direction: row;
+}
+
+
+.seccion2 {
+    width: max-content;
+    display: flex;
+    flex-direction: row;
+}
+
+@media screen and (max-width:1068px) {
+
+    .seccion2 {
+        width: 100%;
+    }
+
+    .contenido {
+        gap: 25px;
+        flex-direction: column;
+    }
+
+}
+
+@media screen and (max-width:798px) {
+
+    .seccion1 {
+        width: 100%;
+        gap: 25px;
+        flex-direction: column;
+    }
+
+    .seccion2 {
+        width: 100%;
+    }
+
+    .contenido {
+        overflow: scroll;
+        gap: 25px;
+        flex-direction: column;
+    }
+
+}
+</style>
